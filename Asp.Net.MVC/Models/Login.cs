d1066171e4bc98f0c430b5b7753102bcc45cbeb7
ihsan-guc <ihsanguc.33@gmail.com>
2023-02-04 16:38:40 +0300

    Gitignore Added.

---
+++ Asp.Net.MVC/Models/Login.cs
@@ -0,0 +1,11 @@
+﻿namespace Asp.Net.MVC.Models
+{
+    public class Login
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public int Like { get; set; }
+    }
+}
---
+++ Asp.Net.MVC/Models/LoginContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Asp.Net.MVC.Models
+{
+    public class LoginContext : DbContext
+    {
+        public LoginContext() : base("LoginContext")
+        {
+
+        }
+        public DbSet<Login> Logins { get; set; }
+    }
+}
